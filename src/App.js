90cbce5bc05214378bcbe6087d5effd197c74f64
anjuli kajawadekar <anjulikajawadekar@gmail.com>
2023-02-07 16:20:08 +0530

    add npm install

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import Login1 from './ComponentFirst/LoginPage';
 import ReqView from './ComponentFirst/ReqView';
 import { Navbar } from 'react-bootstrap';
 import NavBar from './ComponentFirst/NavBar';
-import mobileScreen from './ComponentFirst/NavDemo1';
+// import mobileScreen from './ComponentFirst/NavDemo1';
 import SignUp from './ComponentFirst/SignUp';
 import AddRequisition from './ComponentFirst/AddRequisition';
+import AddCandidate from './ComponentFirst/AddCandidate';
 
 function App() {
   return (
@@ -19,9 +20,10 @@ function App() {
         <Routes>
         <Route path="/" element={<Login1 />}></Route>
         <Route path="/req_view2" element={<ReqView />}></Route>
-        <Route path="/navbar" element={<NavBar />}></Route>
+        {/* <Route path="/navbar" element={<NavBar />}></Route> */}
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/addRequisition" element={<AddRequisition />}></Route>
+        <Route path="/addCandidate" element={<AddCandidate />}></Route>
         {/* <Route path="/navdemo1" element={<mobileScreen />}></Route> */}
         </Routes>
       </Router>
